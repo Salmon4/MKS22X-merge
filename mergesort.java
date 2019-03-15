6b6e96e9ef6f3cee9b3da5356dbfdd23c85555c6
Brandon <bchen00@stuy.edu>
2019-03-15 10:17:24 -0400

    started working on merge

---
+++ mergesort.java
@@ -0,0 +1,15 @@
+public class mergesort{
+	public static void mergesort(int[]data){
+	
+	}
+
+	private static void mergesortHelper(data,lo,hi){
+ 		if (lo >= hi){ 
+    			return;
+		} 
+		//int[] left = new int[data.length/2];
+		//int[] right = new int[data.length - (data.length/2)];
+  		mergesortHelper(data,0,
+  		mergesort right side
+  		merge 
+}
